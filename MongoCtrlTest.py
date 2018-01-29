bc7220f3d747a996e243c1c28d559e95a2af0777
GreenHand <kunwu1997@qq.com>
2018-01-29 21:53:27 +0800

    Add files via upload

---
+++ MongoCtrlTest.py
@@ -0,0 +1,91 @@
+from mongoengine import *
+
+Info = {'addr': '127.0.0.1',
+        'port': 27017,
+        'database': 'Sina'}
+
+
+class UserInfo(Document):
+    """ 个人信息 """
+    _id = StringField()  # 用户ID
+    NickName = StringField()  # 昵称
+    Gender = StringField()  # 性别
+    Province = StringField()  # 所在省
+    City = StringField()  # 所在城市
+    Signature = StringField()  # 个性签名
+    Birthday = DateTimeField()  # 生日
+    Num_Tweets = IntField()  # 微博数
+    Num_Follows = IntField()  # 关注数
+    Num_Fans = IntField()  # 粉丝数
+    Sex_Orientation = StringField()  # 性取向
+    Marriage = StringField()  # 婚姻状况
+    URL = StringField()  # 首页链接
+    meta = {'collection': 'Information'}
+
+    def get_id(self):
+        return self._id
+
+
+class Fans(DynamicDocument):
+    """粉丝信息"""
+    _id = StringField()
+    meta = {'collection': 'Fans'}
+
+    def get_id(self):
+        return self._id
+
+    def get_items(self):
+        """返回所有Fans的列表，第一个值是_id"""
+        items = [self._id]
+        for i in range(1, 200):
+            try:
+                items.append(self[str(i)])
+            except KeyError:
+                break
+        return items
+
+
+class Follows(DynamicDocument):
+    """关注的人信息"""
+    _id = StringField()
+    meta = {'collection': 'Follows'}
+
+    def get_id(self):
+        return self._id
+
+    def get_items(self):
+        """返回所有Follows的列表，第一个值是_id"""
+        items = [self._id]
+        for i in range(1, 200):
+            try:
+                items.append(self[str(i)])
+            except KeyError:
+                break
+        return items
+
+
+class Tweets(Document):
+    """ 微博信息 """
+    _id = StringField()  # 用户ID-微博ID
+    ID = StringField()  # 用户ID
+    Content = StringField()  # 微博内容
+    PubTime = DateTimeField()  # 发表时间
+    Co_oridinates = StringField()  # 定位坐标
+    Tools = StringField()  # 发表工具/平台
+    Like = IntField()  # 点赞数
+    Comment = IntField()  # 评论数
+    Transfer = IntField()  # 转载数
+
+    def get_id(self):
+        return self._id
+
+
+if __name__ == '__main__':
+    connect(
+        Info['database'],
+        host=Info['addr'],
+        port=Info['port'],
+    )
+    for fan in Fans.objects[:1]:
+        itemList = fan.get_items()
+        print(itemList)
