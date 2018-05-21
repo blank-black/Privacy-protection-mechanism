5277fb938e7766349ba42de7e4ea4a57b0a1f237
blank-black <497256303zby@gmail.com>
2018-05-21 22:56:42 +0800

    Update from server

--- flask_time.py
+++ flask_time.py
@@ -5,6 +5,13 @@ from mongoengine import *
 from math import *
 from sklearn.cluster import KMeans
 import re
+import math
+import sys
+import time
+
+DEBUG = 1
+
+max_bias = 0
 
 
 class UserInfo(Document):
@@ -56,8 +63,16 @@ class UserInfo(Document):
             self.tweets_timer.append([secs, 0])
             tweets_num+=1
 
+
         # print(self._id,tweets_num)            
         kmeans = KMeans(n_clusters=3).fit(self.tweets_timer)
+
+        if DEBUG:
+            for i in range(3):
+                h=int(kmeans.cluster_centers_[i][0]//3600)
+                m=int((kmeans.cluster_centers_[i][0]-h*3600)//60)
+                s=int(kmeans.cluster_centers_[i][0]-h*3600-m*60)
+                print('第'+str(i+1)+'个聚类中心为:'+"{:0>2d}".format(h)+':'+"{:0>2d}".format(m)+':'+"{:0>2d}".format(s))
 # cluster_centers_ : array, [n_clusters, n_features]
 # Coordinates of cluster centers
 # labels_ : :
@@ -74,15 +89,18 @@ class UserInfo(Document):
         for tweet in tweets:
             secs=tweet.sec()
 
+
+
             bias = 1/((1/abs(kmeas.cluster_centers_[0][0]-secs+0.01))+(1/abs(kmeas.cluster_centers_[1][0]-secs+0.01))+(1/abs(kmeas.cluster_centers_[2][0]-secs+0.01)))
 
             if bias > maxbias:
                 maxbias=bias
             bias_arr.append(bias)
 
-        for i in range(len(bias_arr)):
-            bias_arr[i]/=maxbias
+        # for i in range(len(bias_arr)):
+        #     bias_arr[i]/=maxbias
 
+        # print(maxbias)
 
         return bias_arr
 
@@ -208,6 +226,39 @@ class UserInfo(Document):
         return score_b
 
 
+    def get_follows(self):
+        follows = Follows.objects(_id__startswith=self._id)
+        return follows
+
+    def get_users(self):
+        UserInfo = UserInfo.objects(_id__startswith=self._id)
+        return UserInfo
+
+
+    def if_abnormal_login(self):
+        if self.if_tweets_less_than_3(self.get_tweets()):
+            return 0
+
+        kmeans=self.tweets_timer_cluster(self.get_tweets())
+        now_time=time.localtime(time.time()).tm_hour*3600+time.localtime(time.time()).tm_min*60+time.localtime(time.time()).tm_sec
+        bias = 1/((1/abs(kmeans.cluster_centers_[0][0]-now_time+0.01))+(1/abs(kmeans.cluster_centers_[1][0]-now_time+0.01))+(1/abs(kmeans.cluster_centers_[2][0]-now_time+0.01)))
+        return if_abnormal_time(bias)
+
+    # def check_before_login(self):
+    #     follows=self.get_follows()
+    #     user=self.get_users()
+    #     follow_user=[]
+    #     Followsnum=0
+    #     for i in range(len(follows.meta)):
+    #         for j in range(len(user._id)):
+    #             if follows[i].meta == user[j]._id:
+    #                 Followsnum += 1
+    #                 follow_user.append(user[j].NickName)
+    #     if Followsnum >= 2:#大于两个关注的人
+    #         random.randint(0, Followsnum-1)
+
+
+
 class Tweets(Document):
     """ 微博信息 """
     _id = StringField()  # 用户ID-微博ID
@@ -282,11 +333,89 @@ class Tweets(Document):
         return a
 
 
+
 class Follows(DynamicDocument):
     """关注的人信息"""
     _id = StringField()
     meta = {'collection': 'Follows'}
 
+
+
+def fisher_cluster(data_list):
+    sum_cost = sys.maxsize
+    label = 0
+    for i in range(1, len(data_list)):
+        d1 = 0
+        d2 = 0
+        avg1 = get_average(data_list[0:i])
+        avg2 = get_average(data_list[i:len(data_list)])
+        for j in range(i):
+            d1 += (data_list[j] - avg1) * (data_list[j] - avg1)# * (data_list[j] - avg1)
+        for j in range(i, len(data_list)):
+            d2 += (data_list[j] - avg2) * (data_list[j] - avg2)# * (data_list[j] - avg2)
+        if d1 + d2 < sum_cost:
+            sum_cost = d1 + d2
+            label = i
+    return label, sum_cost
+
+
+def get_average(inlist):
+    sum_inlist = 0
+    for item in inlist:
+        sum_inlist += item
+    return sum_inlist / len(inlist)
+
+
+def get_Threshold_label():
+    global max_bias
+    file_obj_max = open('./time_clo_max.txt','r').read()
+    file_obj = open('./time_clo.txt','r').read()
+    re_pattern = '\d+'
+    match = re.findall(re_pattern,file_obj)
+    matchmax = re.findall(re_pattern,file_obj_max)
+    max_bias = int(match[0])
+    data = []
+    for d in match:
+        data.append(int(d))
+    label = fisher_cluster(data)[0]
+    return label
+
+def if_abnormal_time(bias):
+    Threshold = get_Threshold_label()*max_bias/100
+    if DEBUG:
+        print("阈值为:"+str(Threshold))
+        print("登录时误差为:"+str(bias))
+
+    if bias > Threshold:
+        return 1
+    return 0
+
+
+def write_bias():
+    bias=[]
+    global max_bias
+    for user in UserInfo.objects:
+        if user.if_tweets_less_than_3(user.get_tweets()) == 0:
+            temp_bias=user.tweets_timer_bias(user.get_tweets())
+            for i in range(len(temp_bias)):
+                bias.append(temp_bias[i])
+
+    bias_clo=[]
+    for i in range(101):
+        bias_clo.append(0)
+
+    for i in range(len(bias)):
+        if bias[i] > max_bias:
+            max_bias=bias[i]
+
+    for i in range(len(bias)):
+        bias_clo[int(bias[i]/max_bias/0.01)]+=1
+
+    fw=open('time_clo.txt','w')
+    print >> fw,bias_clo
+
+    fwmax=open('time_clo_max.txt','w')
+    print >> fwmax,max_bias
 
 app = Flask(__name__)  
 
@@ -300,10 +429,13 @@ def index():
 # @app.route('/username', methods=['GET', 'POST'])
 if __name__ == '__main__': 
     connect('Sina', host='localhost', port=27017)
-    bias=[]
-    for user in UserInfo.objects[:10]:
-        if user.if_tweets_less_than_3(user.get_tweets()) == 0:
-            temp_bias=user.tweets_timer_bias(user.get_tweets())
-            for i in range(len(temp_bias)):
-                bias.append(temp_bias[i])
-    print(bias)
+    # write_bias()
+    name='5053317120'
+    user=UserInfo.objects(_id=name)
+    print('当前系统时间:'+time.asctime( time.localtime(time.time()) ))
+    for u in user:
+        if u.if_abnormal_login():
+            print('登录时误差大于阈值,判断为非正常时间登录')
+        else:
+            print('登录时误差小于等于阈值,判断为正常时间登录')
+
